277efa756206a01d20319559f1746cb801b53f6e
Leoniel Liviero <leolivi@me.com>
2025-12-14 18:36:45 +0100

    chore: outsource handle before request logic and catch send message errors

--- src/background/service-worker.ts
+++ src/background/service-worker.ts
@@ -1,6 +1,6 @@
 /// <reference types="chrome" />
 
-import {TRACKING_DOMAINS} from "../data/tracking-domains";
+import {handleBeforeRequest} from "./handle-before-request";
 
 // ---- IN-MEMORY CACHE ---- //
 let trackersCache: Map<number, Set<string>> = new Map();
@@ -39,55 +39,32 @@ chrome.webRequest.onBeforeRequest.addListener(
 
     if (details.url.includes("chrome-extension://")) return;
 
-    handleBeforeRequest(tabId, details);
+    handleBeforeRequest({
+      tabId,
+      details,
+      trackersCache,
+      onTrackerDetected: (count) => {
+        // save tracker count to storage
+        chrome.storage.local.set({[`trackers_${tabId}`]: count});
+        // notify content script
+        chrome.tabs
+          .sendMessage(tabId, {
+            type: "NETWORK_TRACKER_DETECTED",
+            count,
+          })
+          .catch(() => {
+            console.warn("No content script available in this tab");
+          });
+      },
+    });
     return undefined;
   },
   {urls: ["<all_urls>"]}
 );
 
-function handleBeforeRequest(
-  tabId: number,
-  details: chrome.webRequest.OnBeforeRequestDetails
-): void {
-  try {
-    const url = new URL(details.url);
-
-    // check if request is a tracker
-    const isTracker = TRACKING_DOMAINS.find((tracker) =>
-      url.hostname.includes(tracker.domain)
-    );
-
-    if (!isTracker) return;
-
-    // set tabId if none is set already
-    if (!trackersCache.has(tabId)) {
-      trackersCache.set(tabId, new Set());
-    }
-
-    const trackerSet = trackersCache.get(tabId)!;
-    const trackerDomain = isTracker.domain;
-
-    if (trackerSet.has(trackerDomain)) {
-      return;
-    }
-
-    // increment in-memory counter of trackers
-    trackerSet.add(trackerDomain);
-
-    // save tracker count to storage
-    chrome.storage.local.set({
-      [`trackers_${tabId}`]: trackerSet.size,
-    });
-
-    // notify content script
-    chrome.tabs.sendMessage(details.tabId, {
-      type: "NETWORK_TRACKER_DETECTED",
-      count: trackerSet.size,
-    });
-  } catch (e) {
-    console.error("invalid url", e);
-  }
-}
+/* ---- Tracking Type: 
+NETWORK TRACKER (Request-Level Tracking)
+---- */
 
 /* --- Cleanup --- */
 // remove tracking count if tab is closed
